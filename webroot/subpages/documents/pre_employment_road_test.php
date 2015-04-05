8e034ae3264f7162473e67320bdcdcd4c8b4e3c5
Rob <justdoit2045@gmail.com>
2015-04-05 17:06:18 +0545

    changes

---
+++ webroot/subpages/documents/pre_employment_road_test.php
@@ -0,0 +1,149 @@
+<?php
+ if($this->request->session()->read('debug')){  echo "<span style ='color:red;'>subpages/documents/mee_attach.php #INC204</span>";}
+ ?>
+<form id="form_tab17">
+        <input type="hidden" class="document_type" name="document_type" value="Pre Employment Road Test"/>
+        <input type="hidden" name="sub_doc_id" value="17" class="sub_docs_id" id="af" />
+        <div class="clearfix"></div>
+        <hr/>
+        
+        
+        <div>
+            <div class="col-md-4">
+                    <label class="control-label col-md-4">Driver : </label>  
+                    <div class="col-md-8">              
+                        <input class="form-control" name="driver" />
+                    </div>
+            </div>
+
+            <div class="col-md-4">
+                    <label class="control-label col-md-4">Evaluator : </label>  
+                    <div class="col-md-8">              
+                        <input class="form-control" name="evaluator" />
+                    </div>
+            </div> 
+            <div class="col-md-4">
+                    <label class="control-label col-md-4">Date : </label>  
+                    <div class="col-md-8">              
+                        <input class="form-control" name="date" />
+                    </div>
+            </div> 
+        </div> 
+        <div class="clearfix"></div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c1" /> Pre Trip Inspection performed as per GFS policy
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c2" /> Applies 4 steps to proper coupling
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c3" /> Uses the 5 Keys to Defensive Driving
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <div class="col-md-12">
+                <input type="checkbox" name="c4" /> Aim High In Steering&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Looking 15 seconds ahead
+                <br />
+                <input type="checkbox" name="c5" /> Get The Big Picture&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Check mirrors every 5 to 8 seconds
+                <br />
+                <input type="checkbox" name="c6" /> Keep Your Eyes Moving&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Every 2 seconds, avoid stares
+                <br />
+                <input type="checkbox" name="c7" /> Leave Yourself An Out&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Surround Yourself With Space
+                <br />
+                <input type="checkbox" name="c8" /> Make Sure They See You&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Eye Contact, Tap of the Horn
+                
+            </div>
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c9" /> Keeps following distance of 1 sec per 10 ft of vehicle
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c10" /> Uses brakes smoothly, start braking early to avoid hard stops
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c11" /> Smooth shifting, using proper gear, and down shifts
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c12" /> Operating in Traffic
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <div class="col-md-12">
+                <input type="checkbox" name="c13" /> Uses signals properly
+                <br />
+                <input type="checkbox" name="c14" /> Obeys traffic sings and signals
+                <br />
+                <input type="checkbox" name="c15" /> Intersections
+                <br />
+                <div class="col-md-12">
+                    <input type="checkbox" name="c16" /> Stops ahead of crosswalk
+                    <br />
+                    <input type="checkbox" name="c17" /> Yields right of way
+                    <br />
+                    <input type="checkbox" name="c18" /> Looks left, right and left
+                    <br />
+                </div>                
+            </div>
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c19" /> Passes only when safe to do so
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c20" /> Uses right lane as a habit
+        </div>
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c21" /> Observes speed limit
+        </div>
+        
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c22" /> Courteous to other drivers and pedestrians
+        </div>
+        
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <input type="checkbox" name="c23" /> Backing
+        </div>
+        
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <div class="col-md-12">
+                    <input type="checkbox" name="c24" /> Avoid blind side backing whenever possible
+                    <br />
+                    <input type="checkbox" name="c25" /> Uses horn and hazards
+                    <br />
+                    <input type="checkbox" name="c26" /> Keeps eye on both sides of truck
+                    <br />
+                    <input type="checkbox" name="c27" /> Get out and look to ensure clearance
+                    <br />
+            </div>
+        </div>
+        
+        <p>&nbsp;</p>
+        <div class="col-md-12">
+            <label class="col-md-3">Comments</label>
+            <div class="col-md-6">
+                <textarea name="comment" class="form-control"></textarea>
+            </div>
+        </div>
+        
+        
+                    
+                
+        <div class="clearfix"></div>
+    
+
+</form>
+<script>
+
+</script>
