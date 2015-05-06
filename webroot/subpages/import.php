161a88a569be100927ac6e30d73cf6eb734d45f2
trino <minesh@trinoweb.com>
2015-05-06 13:09:08 -0400

    Roy 1:05

--- webroot/subpages/import.php
+++ webroot/subpages/import.php
@@ -1,3 +1,10 @@
+<?php
+$debug=$this->request->session()->read('debug');
+if($debug) {
+    echo "<span style ='color:red;'>subpages/import.php #INC???</span>";
+}
+?>
+
 <form action="<?php echo $this->request->webroot."profiles/csv";?>" method="post" enctype="multipart/form-data" name="form1" id="form1">
   Choose your file: <br />
   <input name="csv" type="file" id="csv" />
