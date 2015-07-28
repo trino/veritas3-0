7ba645ca77249b4bd264624df40b392f59aeb961
trino <minesh@trinoweb.com>
2015-07-28 15:13:42 -0400

    Roy 3:10
    SIN not required. Good thing too, you can only SIN so much

--- webroot/subpages/profile/info.php
+++ webroot/subpages/profile/info.php
@@ -829,7 +829,7 @@ $strings = CacheTranslations($language, array("forms_%"), $settings);
                                     <div class="form-group">
                                         <label class="control-label"><?= $strings["forms_sin"]; ?>: </label>
                                         <input <?php echo $is_disabled ?> name="sin" type="text"
-                                                                          class="form-control req_driver" <?php if (isset($p->sin)) { ?>
+                                                                          class="form-control" <?php if (isset($p->sin)) { ?>
                                             value="<?php echo $p->sin; ?>" <?php } ?> />
                                     </div>
                                 </div>
