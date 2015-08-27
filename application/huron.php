07e248e45a6b85be3ddf528451fb4db2c9eb1ad3
trino <minesh@trinoweb.com>
2015-08-27 11:09:17 -0400

    Roy 11:05
    big restore

---
+++ application/huron.php
@@ -0,0 +1,11 @@
+<STYLE>
+    .nothuron{
+        display: none; !important;
+    }
+</STYLE>
+<?php
+    $ClientID=30;//41 on live!!!
+    $ChecksNotNeeded = true;
+    $NoClass = "nohuron";
+    include("apply.php");
+?>
